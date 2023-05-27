4152064e9b1d58a7b224f76c01ba796f8006859c
Nikos Tsiknakis <tsiknakisn@gmail.com>
2023-05-27 19:45:06 +0300

    return -100 instead of None label when not used

--- wsi_data/datasets/h5_datasets.py
+++ wsi_data/datasets/h5_datasets.py
@@ -87,9 +87,10 @@ class FeatureDatasetHDF5(Dataset):
 
             if "labels" in self.data_cols:
                 label = h5_dataset[self.data_cols["labels"]][0] - self.base_label
-                label = torch.from_numpy(np.array([label], dtype=label.dtype))
             else:
-                label = None
+                label = -100
+
+            label = torch.from_numpy(np.array([label], dtype=label.dtype))
 
             features = dict(features=features)
 
