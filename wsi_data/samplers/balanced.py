a3c303c469aef2702d45557ba88108c15ebf4748
Nikos Tsiknakis <tsiknakisn@gmail.com>
2023-11-30 12:38:36 +0200

    specify label key

--- wsi_data/samplers/balanced.py
+++ wsi_data/samplers/balanced.py
@@ -4,12 +4,12 @@ import torch
 from torch.utils.data import BatchSampler
 
 
-def get_weighted_random_sampler(dataset):
+def get_weighted_random_sampler(dataset, label_key: str = "labels"):
     """
     Returns a WeightedRandomSampler for a given dataset.
     The weights are calculated by taking the inverse of the class frequency.
     """
-    labels, class_sample_count = dataset.get_label_distribution()
+    labels, class_sample_count = dataset.get_label_distribution(label_key=label_key)
     weight = 1.0 / class_sample_count.float()
     samples_weight = torch.tensor([weight[t] for t in labels])
     return torch.utils.data.WeightedRandomSampler(samples_weight, len(samples_weight))
